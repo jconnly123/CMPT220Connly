f303ded0565d1788e10d19ead9c4c40d2d97ed45
jconnly123 <jack.connly1@marist.edu>
2016-12-14 14:38:35 -0500

    simple commit
    JACK CONNLY

--- Labs/lab 9/12.3/ArrayIndex.java
+++ Labs/lab 9/12.3/ArrayIndex.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class ArrayIndex {
   public static void main(String[] args) {
     int[] array = new int[100];
-      for (int i = 0; i < array.length; i++) {
+    for (int i = 0; i < array.length; i++) {
           array[i] = (int) (Math.random() * 100);
       }
       Scanner input = new Scanner(System.in);
